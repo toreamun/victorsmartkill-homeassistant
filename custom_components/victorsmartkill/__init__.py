02e8cec0c565a1aa53986d5d277cfc67fa7658a9
tore <tore@amundsen.org>
2020-10-30 19:25:52 +0000

    Refactored to make testing easier

--- custom_components/victorsmartkill/__init__.py
+++ custom_components/victorsmartkill/__init__.py
@@ -38,55 +38,40 @@ async def async_setup(hass: HomeAssistantType, config: Config) -> bool:
 
 async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry) -> bool:
     """Set up this integration using UI."""
-    _LOGGER.debug(
-        "Setup entry %s %s with scan interval %s.",
-        entry.domain,
-        entry.title,
-        SCAN_INTERVAL,
-    )
+    _LOGGER.debug("async_setup_entry %s.", entry.title)
 
     if hass.data.get(DOMAIN) is None:
         hass.data.setdefault(DOMAIN, {})
         _LOGGER.info(STARTUP_MESSAGE)
 
-    username = entry.data.get(CONF_USERNAME)
-    password = entry.data.get(CONF_PASSWORD)
-
-    coordinator = VictorSmartKillDataUpdateCoordinator(
-        hass,
-        SCAN_INTERVAL,
-        username=username,
-        password=password,
-    )
-    await coordinator.async_refresh()
-
-    if not coordinator.last_update_success:
-        raise ConfigEntryNotReady
-
+    coordinator = await _async_initialize_coordinator(hass, entry)
     hass.data[DOMAIN][entry.entry_id] = coordinator
-
-    # Forward setup to each platform
-    for platform in PLATFORMS:
-        if entry.options.get(platform, True):
-            coordinator.platforms.append(platform)
-            # Use `hass.async_create_task` (not async_add_job) to avoid a circular
-            # dependency between the platform and the component
-            hass.async_create_task(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
-
-    entry.add_update_listener(async_reload_entry)
-
-    @callback
-    async def async_trap_list_changed(event: EventType):
-        _LOGGER.info(
-            "Trap list hast changed (%s), reload integration entry.", event.data
-        )
-        await async_reload_entry(hass, entry)
-
-    hass.bus.async_listen_once(EVENT_TRAP_LIST_CHANGED, async_trap_list_changed)
+    await _async_forward_platform_setup(hass, entry, coordinator)
+    _setup_reload_triggers(hass, entry)
 
     return True
+
+
+async def async_unload_entry(hass: HomeAssistantType, entry: ConfigEntry):
+    """Handle removal of an entry."""
+    _LOGGER.debug("async_unload_entry %s.", entry.title)
+
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    is_unloaded = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, platform)
+                for platform in coordinator.platforms
+            ]
+        )
+    )
+
+    if is_unloaded:
+        coordinator = hass.data[DOMAIN].pop(entry.entry_id)
+        if coordinator:
+            await coordinator.async_close()
+
+    return is_unloaded
 
 
 class VictorSmartKillDataUpdateCoordinator(DataUpdateCoordinator[List[Trap]]):
@@ -95,65 +80,37 @@ class VictorSmartKillDataUpdateCoordinator(DataUpdateCoordinator[List[Trap]]):
     def __init__(
         self,
         hass: HomeAssistantType,
+        logger: logging.Logger,
         update_interval: timedelta,
         username: str,
         password: str,
+        platforms: List[str],
     ) -> None:
         """Initialize."""
+        self.platforms: List[str] = platforms
         self._client = VictorAsyncClient(username, password)
         self._api = VictorApi(self._client)
-        self.platforms: List[str] = []  # appended from async_setup_entry
 
         super().__init__(
             hass,
-            _LOGGER,
+            logger,
             update_method=self.async_update_data,
             name=DOMAIN,
             update_interval=update_interval,
         )
 
-    async def async_close(self) -> None:
-        """Close resources."""
-        self.logger.debug("Close API client.")
-        await self._client.aclose()
-
-    @callback
-    def async_add_listener(
-        self, update_callback: Callable[[], None]
-    ) -> Callable[[], None]:
-        """Listen for data updates. Called by CoordinatorEntity."""
-        self.logger.debug(
-            "START: async_add_listener()",
-        )
-        try:
-            return super().async_add_listener(update_callback)
-        except Exception:
-            self.logger.debug("async_add_listener() failed", exc_info=True)
-            raise
-        finally:
-            self.logger.debug(
-                "END: async_add_listener()",
-            )
-
-    async def async_refresh(self) -> None:
-        """Refresh data."""
-        try:
-            await super().async_refresh()
-        except Exception:
-            self.logger.debug("async_refresh() failed", exc_info=True)
-            raise
-        self.logger.debug(
-            "async_refresh() done, last_update_success is %s", self.last_update_success
+        logger.info(
+            (
+                "Data update coordinator for Victor Smart-Kill account %s "
+                "initialized with %s platforms and update interval %s."
+            ),
+            username,
+            platforms,
+            update_interval,
         )
 
     async def async_update_data(self) -> List[Trap]:
         """Update data via Victor Smart-Kill API."""
-        self.logger.debug(
-            "async_update_data running for %s, currently coordinating %d listeners.",
-            type(self).__name__,
-            len(self._listeners),
-        )
-
         try:
             if not self.data:
                 traps = await self._get_traps()
@@ -179,12 +136,27 @@ class VictorSmartKillDataUpdateCoordinator(DataUpdateCoordinator[List[Trap]]):
         except Exception as exception:
             raise UpdateFailed(exception) from exception
 
+    @callback
+    def async_add_listener(
+        self, update_callback: Callable[[], None]
+    ) -> Callable[[], None]:
+        """Listen for data updates. Called by CoordinatorEntity."""
+        # Overrided to add debug logging
+        remove_listener_callback = super().async_add_listener(update_callback)
+        self.logger.debug("Listener %s added to coordinator.", update_callback.__name__)
+        return remove_listener_callback
+
+    async def async_close(self) -> None:
+        """Close resources."""
+        self.logger.debug("Close API client.")
+        await self._client.aclose()
+
     async def _get_traps(self) -> List[Trap]:
         """Get list of traps from API."""
         try:
             traps = await self._api.get_traps()
             self.logger.debug(
-                "Received traps %s from Victor Smart-Kill API.",
+                "Received traps list %s from Victor Smart-Kill API.",
                 sorted([trap.id for trap in traps]),
             )
             return traps
@@ -195,28 +167,62 @@ class VictorSmartKillDataUpdateCoordinator(DataUpdateCoordinator[List[Trap]]):
             raise
 
 
-async def async_unload_entry(hass: HomeAssistantType, entry: ConfigEntry):
-    """Handle removal of an entry."""
-    _LOGGER.debug("Unload entry %s %s.", entry.domain, entry.title)
-    coordinator = hass.data[DOMAIN][entry.entry_id]
-    unloaded = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, platform)
-                for platform in PLATFORMS
-                if platform in coordinator.platforms
-            ]
-        )
+async def _async_initialize_coordinator(
+    hass: HomeAssistantType, entry: ConfigEntry
+) -> VictorSmartKillDataUpdateCoordinator:
+    username = entry.data.get(CONF_USERNAME)
+    password = entry.data.get(CONF_PASSWORD)
+    enabled_platforms = [
+        platform for platform in PLATFORMS if entry.options.get(platform, True)
+    ]
+
+    coordinator = VictorSmartKillDataUpdateCoordinator(
+        hass, _LOGGER, SCAN_INTERVAL, username, password, enabled_platforms
     )
-    if unloaded:
-        coordinator = hass.data[DOMAIN].pop(entry.entry_id)
-        if coordinator:
-            await coordinator.async_close()
 
-    return unloaded
+    # Initialize coordinator with trap data
+    await coordinator.async_refresh()
+    if not coordinator.last_update_success:
+        raise ConfigEntryNotReady
+
+    return coordinator
 
 
-async def async_reload_entry(hass: HomeAssistantType, entry: ConfigEntry):
+async def _async_forward_platform_setup(
+    hass: HomeAssistantType,
+    entry: ConfigEntry,
+    coodinator: VictorSmartKillDataUpdateCoordinator,
+):
+    """Forward setup to each platform."""
+    for platform in coodinator.platforms:
+        _LOGGER.debug("Forward setup to %s platform.", platform)
+        # Use `hass.async_create_task` to avoid a circular
+        # dependency between the platform and the component
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, platform)
+        )
+
+
+def _setup_reload_triggers(hass: HomeAssistantType, entry: ConfigEntry):
+    """Set up listeners of reload triggers."""
+    # Listen for config entry changes and reload when changed.
+    @callback
+    async def async_config_entry_changed(hass: HomeAssistantType, entry: ConfigEntry):
+        _LOGGER.info("Config entry has change. Reload integration.")
+        await _async_reload_entry(hass, entry)
+
+    entry.add_update_listener(async_config_entry_changed)
+
+    # Listen for trap list changes and reload when changed (new traps etc.)
+    @callback
+    async def async_trap_list_changed(event: EventType):
+        _LOGGER.info("Trap list hast changed (%s). Reload integration.", event.data)
+        await _async_reload_entry(hass, entry)
+
+    hass.bus.async_listen_once(EVENT_TRAP_LIST_CHANGED, async_trap_list_changed)
+
+
+async def _async_reload_entry(hass: HomeAssistantType, entry: ConfigEntry):
     """Reload entry."""
     await async_unload_entry(hass, entry)
     await async_setup_entry(hass, entry)
